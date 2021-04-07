f9b6cd90b02e996afed66c58e4340e5938713097
Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>
2021-04-07 14:51:25 +0300

    solving delay

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,25 +9,32 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Maze generator");
 
         Parent root = FXMLLoader.load(getClass().getResource("output.fxml"));
-
-
         Scene scene = new Scene(root, 792, 649);
         scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
 
 
-
-
         primaryStage.setScene(scene);
+
+        ////calculation that needs to be in a non-FX thread
+
+        new Thread()
+        {
+            public void run()
+            {
+                MazeController mc = new MazeController(scene, 10,10);
+                mc.setBackgroundColor();
+                mc.initializeCells();
+                mc.drawCells();
+            }
+        }.start();
+
+
         primaryStage.show();
 
-        MazeController mc = new MazeController(scene, 5,5);
-        mc.setBackgroundColor();
-        mc.initializeCells();
-        mc.drawCells();
 
     }
 
